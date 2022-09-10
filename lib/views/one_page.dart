83ea3530772e2b157e6731feff8d411bcf7afe61
andres-alba <andresalba34@gmail.com>
2022-09-10 11:24:54 -0500

    add sort dropdown and blog display

--- lib/views/one_page.dart
+++ lib/views/one_page.dart
@@ -19,145 +19,155 @@ class _OnePageState extends State<OnePage> {
 
   final controller = PageController();
   bool isLastPage = false;
+  String dropdownvalue = 'Recent';
+  var items = [
+    'Recent',
+    'Oldest',
+  ];
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: ListView(
-          children: [
-            const SizedBox(height: 80),
-            const Text('One Page',
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontFamily: 'Poppins',
-                  fontSize: 30,
-                  //wordSpacing: 1.2,
-                  height: 1.3,
-                )),
-            const SizedBox(
-              height: 20,
-            ),
+        body: Container(
+          color: Colors.black87,
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Sort by:',
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                    DropdownButton(
+                      value: dropdownvalue,
+                      items: items.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue = newValue!;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              FutureBuilder<Feed>(
+                future: futureFeed,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  } else {
+                    return Expanded(
+                      child: ListView.separated(
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(
+                                height: 3,
+                              ),
+                          itemCount: snapshot.data!.data!.length,
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            final author = snapshot.data!.data![index];
 
-            FutureBuilder<Feed>(
-              future: futureFeed,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const CircularProgressIndicator();
-                } else {
-                  return Expanded(
-                    child: ListView.separated(
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider(
-                              height: 1,
-                            ),
-                        itemCount: 2,
-                        shrinkWrap: true,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            color: Colors.black87,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                            return Container(
+                              color: Colors.black87,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            CircleAvatar(
+                                              radius: 20.0,
+                                              backgroundImage: NetworkImage(
+                                                author.authorAvatarUrl.toString()
+                                              ),
+                                              backgroundColor:
+                                                  Colors.transparent,
+                                            ),
+                                            const SizedBox(
+                                              width: 5,
+                                            ),
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(author.authorName.toString(),
+                                                  style: const TextStyle(
+                                                      color: Colors.red),
+                                                ),
+                                                Text(
+                                                  author.timestamp.toString(),
+                                                  style: const TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                              ],
+                                            ),
+                                            //  Text(snapshot.data!.data![index].),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          author.title != null || author.title == ""
+                                              ? author.title.toString()
+                                              : 'No POST',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.w700,
+                                              color: Colors.white,
+                                              fontSize: 18),
+                                        ),
+                                        Text(
+                                          author.text.toString(),
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 14),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const Divider(
+                                    color: Colors.white,
+                                  ),
+                                  Row(
                                     children: [
-                                      Row(
-                                        children: [
-                                          CircleAvatar(
-                                            radius: 20.0,
-                                            backgroundImage: NetworkImage(
-                                                snapshot.data!.data![index]
-                                                    .authorAvatarUrl
-                                                    .toString()),
-                                            backgroundColor: Colors.transparent,
-                                          ),
-                                          const SizedBox(width: 5,),
-                                          Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                snapshot.data!.data![index]
-                                                    .authorName
-                                                    .toString(),
-                                                style: const TextStyle(
-                                                    color: Colors.red),
-                                              ),
-                                              Text(
-                                                snapshot.data!.data![index]
-                                                    .timestamp
-                                                    .toString(),
-                                                style: const TextStyle(
-                                                    color: Colors.white),
-                                              ),
-                                            ],
-                                          ),
-                                          //  Text(snapshot.data!.data![index].),
-                                        ],
+                                      const Icon(
+                                        Icons.thumb_up_sharp,
+                                        color: Colors.grey,
+                                        size: 20,
                                       ),
-                                      const SizedBox(height:5),
-                                      Text(
-                                        snapshot.data!.data![index].title !=
-                                                    null ||
-                                                snapshot.data!.data![index]
-                                                        .title ==
-                                                    ""
-                                            ? snapshot.data!.data![index].title
-                                                .toString()
-                                            : 'No POST',
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.w700,
-                                            color: Colors.white,
-                                            fontSize: 18),
+                                      const SizedBox(
+                                        width: 5,
                                       ),
                                       Text(
-                                        snapshot.data!.data![index].text
-                                            .toString(),
+                                        author.totalPostViews.toString(),
                                         style: const TextStyle(
-                                            color: Colors.white, fontSize: 14),
+                                            color: Colors.grey, fontSize: 12),
                                       ),
                                     ],
                                   ),
-                                ),
-                                const Divider(
-                                  color: Colors.white,
-                                ),
-                                Row(
-                                  children: [
-                                    const Icon(
-                                      
-                                      Icons.thumb_up_sharp,
-                                      color: Colors.grey,
-                                      size: 20,
-                                    ),
-                                    const SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      snapshot.data!.data![index].totalPostViews
-                                          .toString(),
-                                      style: const TextStyle(
-                                          color: Colors.grey, fontSize: 12),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height:10),
-                              ],
-                            ),
-                          );
-                        }),
-                  );
-
-                  //Text(snapshot.data!.data![0].authorId.toString());
-                }
-              },
-
-              // By default, show a loading spinner.
-            )
-          ],
+                                  const SizedBox(height: 10),
+                                ],
+                              ),
+                            );
+                          }),
+                    );
+                  }
+                },
+                // By default, show a loading spinner.
+              )
+            ],
+          ),
         ),
       );
 }
